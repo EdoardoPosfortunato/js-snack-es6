6bdef2251c799742c6667706a7dac25cb24e4535
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-05-05 16:54:24 +0200

    feat: classifica ordinata

--- script/script2.js
+++ script/script2.js
@@ -9,35 +9,73 @@ const squadreCalcio = [
     { nome: "Fiorentina", puntiFatti: 0, falliSubiti: 0 },
     { nome: "Torino", puntiFatti: 0, falliSubiti: 0 },
     { nome: "Bologna", puntiFatti: 0, falliSubiti: 0 }
-  ];
+];
 
-  
-  // funzioni //
-  const numeroRandom = (min, max) => {
+console.log('ciao')
+
+// funzioni //
+const numeroRandom = (min, max) => {
 
     return Math.floor(Math.random() * (max - min + 1)) + min;
 
-  }
-1
+}
 
-  for (let i = 0; i < squadreCalcio.length; i++){
+/////////////////////////////////////////////////////////
+
+for (let i = 0; i < squadreCalcio.length; i++) {
 
     squadreCalcio[i].puntiFatti = numeroRandom(0, 100);
     squadreCalcio[i]['falliSubiti'] = numeroRandom(0, 50);
- 
- }
 
- console.table(squadreCalcio);
+}
 
-const falliSubiti = [];
+console.table(squadreCalcio);
 
- for (let i = 0; i < squadreCalcio.length; i++){
+/* const falliSubiti = [];
 
-    
+for (let i = 0; i < squadreCalcio.length; i++) {
+
+
     falliSubiti.push({
-        nome: squadreCalcio[i].nome, 
+        nome: squadreCalcio[i].nome,
         falli: squadreCalcio[i].falliSubiti
     })
- 
- }
- console.table(falliSubiti)
+
+}
+console.table(falliSubiti) */
+
+// lista ordinata //
+
+const copiaSquadre = [];
+
+for (let i = 0; i < squadreCalcio.length; i++) {
+  // Copia ogni oggetto (spread operator)
+  copiaSquadre.push({ ...squadreCalcio[i] });
+}
+
+// Ordiniamo la copia con selection sort decrescente
+for (let i = 0; i < copiaSquadre.length - 1; i++) {
+  let maxIndex = i;
+  for (let j = i + 1; j < copiaSquadre.length; j++) {
+    if (copiaSquadre[j].puntiFatti > copiaSquadre[maxIndex].puntiFatti) {
+      maxIndex = j;
+    }
+  }
+  let temp = copiaSquadre[i];
+  copiaSquadre[i] = copiaSquadre[maxIndex];
+  copiaSquadre[maxIndex] = temp;
+}
+
+// Aggiungiamo la posizione in classifica
+const classificaSquadre = [];
+
+for (let i = 0; i < copiaSquadre.length; i++) {
+  classificaSquadre.push({
+    posizione: i + 1,
+    nome: copiaSquadre[i].nome,
+    puntiFatti: copiaSquadre[i].puntiFatti
+    
+  });
+}
+
+console.table(classificaSquadre);
